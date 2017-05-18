9fd59c55fa52032aa9ec11ba045e126422310fa5
k.takahashi <tk02dr@gmail.com>
2017-05-19 00:44:56 +0900

    プロトタイプ

--- source/application.js
+++ source/application.js
@@ -1,14 +1,31 @@
 $(function(){
   kongregateAPI.loadAPI(function(){
     window.kongregate = kongregateAPI.getAPI();
-    if(kongregate.services.isGuest()){
-      alert("ok");
+    // if(kongregate.services.isGuest()){
+    //   alert("ok");
+    // }
+  });
+  var point = 0;
+  initilize_text();
+  document.onkeydown = function (e){
+    var target_text  = $(".chara_row").text();
+    var target_chara = target_text.charAt(0);
+    var input_chara  = String.fromCharCode(e.keyCode).toLowerCase();
+    if (input_chara == target_chara){
+      $(".chara_row").text(target_text.substr(1) + get_random_alphabet());
+      point++;
+      $(".point_display").text(point);
     }
-  });
-  var candy = 0;
-  $(".candy_number").text(candy);
-  $(".center_candy").on("click", function(){
-    candy++;
-    $(".candy_number").text(candy);
-  });
+  };
 });
+
+function get_random_alphabet() {
+  return String.fromCharCode(parseInt(Math.random() * 26 + 65)).toLowerCase();
+}
+function initilize_text(){
+  var init_text = "";
+  for(var i = 0; i<19; i++){
+    init_text += get_random_alphabet();
+  }
+  $(".chara_row").text(init_text);
+}
